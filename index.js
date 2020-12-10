c983f33047f8bd4d6c058b0a27fd0a5a506fa3ef
Michael James Rojas <michaeljamesrojas123@gmail.com>
2020-12-10 21:15:32 +0800

    edit

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-// alert("Hello");
+// alert(eval(789))
 
 // Actual codes
 let number = "0";
-let lastDigitPressed = "0";
 let toBeOperated = '0';
-let lastOperatorPressed = "";
-let lastButtonPressedID;
+let answer;
+let lastDigitPressed = undefined;
+let lastOperatorPressed = undefined;
+let lastButtonPressed;
+let lastOperatorOrNumberButtonPressed;
 
+// Color changer
+// let inputElemColorPicker = document.getElementById("calculatorColorPicker");
+// inputElemColorPicker.addEventListener("close", function (){
+//     //  alert("hi");
 
-// Add num buttons functionality
+// });
+
+// Add number buttons functionality
+// * add digit to last item of number typed
+// * replace all numbers if numbers was an answer
 let buttonElementsCollection = document.getElementsByClassName("numbers");
 let buttonElements = Array.from(buttonElementsCollection);
 buttonElements.forEach(elementd => {
@@ -25,6 +35,8 @@ buttonElements.forEach(elementd => {
 });
 
 // Add delete button functionality
+// * delete everylast digit from number
+// * delete whole number if number was an answer before
 let buttElemDelete = document.getElementById("bdelete");
 buttElemDelete.addEventListener("click", function (){
     // alert(buttElemDelete);
@@ -34,6 +46,7 @@ buttElemDelete.addEventListener("click", function (){
     }
     else{
         number = 0;
+        // lastDigitPressed = undefined;
     }
     updateNumberDisplay();
 })
@@ -44,14 +57,19 @@ buttElemClear.addEventListener("click", function (){
     // Reset everything
     number = 0;
     toBeOperated = "0";
+    lastDigitPressed = undefined;
+    lastOperatorPressed = undefined;
     //! 
     // Add reset for memory store and update subdisplay
     updateNumberDisplay();
+    updateSubDisplay();
 })
 
 
 
 // Add operators button functionalites
+//TODO: add numbers to toBeoperated or replace last operator if previous button is still operator
+//TODO: evaluate current toBeOperated
 let buttElemOperatorsCollection = document.getElementsByClassName("operator");
 let buttElemOperators = Array.from(buttElemOperatorsCollection);
 buttElemOperators.forEach(element => {
@@ -60,20 +78,36 @@ buttElemOperators.forEach(element => {
         // alert(element.getAttribute("data-operator"));
         // Put number on sub-display
         lastOperatorPressed = element.getAttribute("data-operator");
-        updateSubDisplay();
+        processOperatorClicked();
     })
 });
 
+
+// Add equal button functionality
+// 
+let buttElemEqual = document.getElementById("bequals");
+buttElemEqual.addEventListener("click", function (){
+    evaluateToBeOperated();
+});
+
+
 // Warning: This function should be last event adition
-// Add lastButtonpressed functionality to all button
+// TODO Add lastButtonpressed functionality to all button
 let buttElemsAllButtonsCollection = document.getElementsByTagName("button");
 let buttElemsAllButtons = Array.from(buttElemsAllButtonsCollection);
 buttElemsAllButtons.forEach( element => {
     element.addEventListener("click",
     function (){
         //T
-        lastButtonPressedID = element.id;
-        alert(lastButtonPressedID);
+        lastButtonPressed = element;
+        // alert(lastButtonPressed.id);
+        if (element.className == "operator") {
+            lastOperatorOrNumberButtonPressed = element;
+        }
+        else if(element.className == "numbers"){
+            lastOperatorOrNumberButtonPressed = element;
+        }
+        // alert(lastOperatorOrNumberButtonPressed.id);
     });
 });
 
@@ -98,17 +132,69 @@ buttElemsAllButtons.forEach( element => {
 
 // Functions
 
+function evaluateToBeOperated(){
+    // if(lastButtonPressed.className == "operator"){
+        toBeOperated = toBeOperated.slice(0, -1);
+    // }
+    // else if
+    alert(toBeOperated);
+    answer = eval(toBeOperated);
+    alert(answer);
+}
+
 function updateSubDisplay(){
-    if(lastButtonPressedID == "b0" && number == '0'){
-        // toBeOperated += number + ;
+    let pElemSubDisplay = document.getElementById("p-subDisplay");
+    pElemSubDisplay.textContent = toBeOperatedActualDisplay(toBeOperated);
+}
+
+//!
+function toBeOperatedActualDisplay(toBeOperatedString){
+    
+    return toBeOperatedString;
+}
+
+//!
+function processOperatorClicked(){
+    
+    
+    if(lastOperatorOrNumberButtonPressed.className == "operator"){
+        if(lastDigitPressed != undefined){
+            //alt toBeOperated
+            // toBeOperated = toBeOperated.slice(0, -2);
+            toBeOperated = toBeOperated.slice(0, -1);
+            //alt toBeOperated
+            // toBeOperated += " " + lastOperatorPressed;
+            toBeOperated += lastOperatorPressed;
+        }
     }
-    alert(lastButtonPressedID);
+    else if(lastOperatorOrNumberButtonPressed.className == "numbers"){
+        
+        if (toBeOperated == "0") {
+            toBeOperated = "";
+        }
+
+        if(lastOperatorOrNumberButtonPressed.id == "b0" && number == '0'){
+            //alt toBeOperated
+            // toBeOperated += " " + number + " " + lastOperatorPressed;
+            toBeOperated += number + lastOperatorPressed;
+        }
+
+        else{
+            //alt toBeOperated
+            // toBeOperated += " " + number + " " + lastOperatorPressed;
+            toBeOperated += number + lastOperatorPressed;
+        }
+    }
+
+    updateSubDisplay();
+    number = '0';
+    updateNumberDisplay();
  }
 
 function addLastDigitPressedToNumber(){
     //! 
     // check the number of digits if valid
-    if (number.length >= 14) {
+    if (number.length >= 20) {
         // alert("max digits reached");
         return;
     }
@@ -159,4 +245,3 @@ function operate(x, y, operator){
     else 
         return undefined;
 }
-
